7dd55f656d83ff5732f966737ce87c3a6f182b25
Milena <milenacurtinhas@gmail.com>
2024-09-15 12:53:47 -0300

    Busca nos arquivos

--- client.c
+++ client.c
@@ -6,44 +6,53 @@
 int main(int argc, char *argv[]) {
     char *diretorio = argv[1];
 
-    char *indexStr = malloc(strlen(diretorio) + strlen("/index.txt") + 1);
-    strcpy(indexStr, diretorio);
-    strcat(indexStr, "/index.txt");
-
-    char *graphStr = malloc(strlen(diretorio) + strlen("/graph.txt") + 1);
-    strcpy(graphStr, diretorio);
-    strcat(graphStr, "/graph.txt");
-
     char *stopwordsStr = malloc(strlen(diretorio) + strlen("/stopwords.txt") + 1);
     strcpy(stopwordsStr, diretorio);
     strcat(stopwordsStr, "/stopwords.txt");
 
+    FILE *stopwordsFile  = fopen(stopwordsStr, "r");
+    TST *stopwordsTST = create_stopwords_tst(stopwordsFile);
+    fclose(stopwordsFile);
+
+    char *indexStr = malloc(strlen(diretorio) + strlen("/index.txt") + 1);
+    strcpy(indexStr, diretorio);
+    strcat(indexStr, "/index.txt");
+
     char *pagesStr = malloc(strlen(diretorio) + strlen("/pages/") + 1);
     strcpy(pagesStr, diretorio);
     strcat(pagesStr, "/pages/");
-    
-    FILE *indexFile  = fopen(indexStr, "r");
-    FILE *graphFile = fopen(graphStr, "r");
-    FILE *stopwordsFile  = fopen(stopwordsStr, "r");
 
-    TST *stopwordsTST = create_stopwords_tst(stopwordsFile);
-    TST *twordsTST = create_twords_tst(indexFile , stopwordsTST, pagesStr);
-    int n = 0;
-    int it = 0;
+    FILE *indexFile  = fopen(indexStr, "r");
+    TST *twordsTST = create_twords_tst(indexFile, stopwordsTST, pagesStr);
+    fclose(indexFile);
+
+    char *graphStr = malloc(strlen(diretorio) + strlen("/graph.txt") + 1);
+    strcpy(graphStr, diretorio);
+    strcat(graphStr, "/graph.txt");
+    
+    FILE *graphFile = fopen(graphStr, "r");
+    int n = 0, it = 0;
     graphTST *graph = create_graph_tst(graphFile, &n);
+    fclose(graphFile);
     graph = pageRankCalc(graph, n, &it);
-    printf("n its: %d\n", it);
+
+    char *searchesStr = malloc(strlen(diretorio) + strlen("/searches.txt") + 1);
+    strcpy(searchesStr, diretorio);
+    strcat(searchesStr, "/searches.txt");
+
+    FILE *searchesFile = fopen(searchesStr, "r");
+    read_searches(searchesFile, stopwordsTST, twordsTST, graph, it);
+    fclose(searchesFile);
     
     free_tst(stopwordsTST);
     free_tst(twordsTST);
     free_tst_graph(graph);
-    free(indexStr);
-    free(graphStr);
+    
     free(stopwordsStr);
+    free(indexStr);
     free(pagesStr);
-    fclose(indexFile);
-    fclose(graphFile);
-    fclose(stopwordsFile);
+    free(graphStr);
+    free(searchesStr);
 
     return 0;
 }
